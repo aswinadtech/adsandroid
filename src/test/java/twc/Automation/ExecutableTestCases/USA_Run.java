2010c2074e0a169aada25558a5da56b640b7b630
aswinadtech <49895659+aswinadtech@users.noreply.github.com>
2022-05-12 10:37:30 +0530

    Update USA_Run.java

--- src/test/java/twc/Automation/ExecutableTestCases/USA_Run.java
+++ src/test/java/twc/Automation/ExecutableTestCases/USA_Run.java
@@ -1917,7 +1917,7 @@ Thread.sleep(100000);
 
 	}
 
-	@Test(priority = 1916, enabled = true)
+	/*@Test(priority = 1916, enabled = true)
 	@Description("Verify amazon aax Feed3 preload ad call when privacy optin")
 	public void Verify_amazon_aax_feed3_adcall_privacy_optin_for_USA() throws Exception {
 		System.out.println("==============================================");
@@ -1929,7 +1929,7 @@ Thread.sleep(100000);
 		
 		////Utils.verifyAAX_SlotId("Smoke", "Feed3", true);
 
-	}
+	}*/
 
 
 
